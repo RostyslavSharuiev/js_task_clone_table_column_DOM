e1b5b334f031c3702f44ad570e37450a7590c20c
Rostyslav <rostislavkpi@gmail.com>
2024-08-14 23:16:49 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const table = document.querySelector("table");
+for (const row of table.rows){
+    const cellsCopy = row.cells[1].cloneNode(true);
+    row.cells[row.cells.length - 1].before(cellsCopy);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
